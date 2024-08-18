6ea0be45a588c51c11b7e23bfd059dc53db49e0e
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2024-08-18 21:15:57 +0330

    fixing lenis

--- components/home/stack-cards/StackCards.tsx
+++ components/home/stack-cards/StackCards.tsx
@@ -1,36 +1,21 @@
 'use client'
 
 import { useScroll } from 'framer-motion'
-import { useEffect, useRef } from 'react'
-import { ReactLenis, useLenis } from 'lenis/react'
+import { useRef } from 'react'
+import { ReactLenis } from 'lenis/react'
 import Card from './Card'
-import { projects } from './data'
-import gsap from 'gsap/all'
-import { rooms, special } from '@/constants'
+
+import { rooms } from '@/constants'
 
 export default function StackCards() {
-  const lenisRef = useRef(null)
   const container = useRef(null)
   const { scrollYProgress } = useScroll({
     target: container,
     offset: ['start start', 'end end'],
   })
 
-  useEffect(() => {
-    function update(time: any) {
-      // @ts-ignore
-      container.current?.lenis?.raf(time * 1000)
-    }
-
-    gsap.ticker.add(update)
-
-    return () => {
-      gsap.ticker.remove(update)
-    }
-  })
-
   return (
-    <ReactLenis ref={lenisRef} autoRaf={false}>
+    <ReactLenis root>
       <section ref={container} className="relative">
         {rooms.map((room, i) => {
           const targetScale = 1 - (rooms.length - i) * 0.05
--- next.config.js
+++ next.config.js
@@ -16,4 +16,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-// mye-commerce.storage.iran.liara.space
